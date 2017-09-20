3b42ee3e15475c71c9d8012092d8b2d9577d807c
Javier Ramila <jandramila@gmail.com>
2017-09-20 20:09:44 -0300

    Adds UDP Client timeout, logging function & bestows client handling.

--- Server.py
+++ Server.py
@@ -4,13 +4,14 @@ import cv2
 import numpy
 from threading import Thread
 import Queue
+from datetime import datetime, timedelta
 
-SERVER_ADDRESS = '192.168.43.218'
+SERVER_ADDRESS = 'localhost'
 UDP_PORT = 10021
 # import code; code.interact(local=dict(globals(), **locals()))
 
-active_clients = []
-encode_params = [int(cv2.IMWRITE_JPEG_QUALITY),30]
+active_udp_clients = dict()
+encode_params = [int(cv2.IMWRITE_JPEG_QUALITY),20]
 
 class UDPClient(Thread):
     def __init__(self, socket, client_address):
@@ -18,20 +19,30 @@ class UDPClient(Thread):
         self.mailbox = Queue.Queue()
         self.socket = socket
         self.address = client_address
-        active_clients.append(self.mailbox)
+        self.last_req_time = datetime.now()
+        active_udp_clients[client_address] = self
 
     def run(self):
         while True:
+            if (datetime.now() - self.last_req_time > timedelta(seconds = 90)):
+                # Check last req time for hanged connections
+                break
             frame = self.mailbox.get()
             if isinstance(frame, basestring) and frame == 'shutdown':
-                print self, 'shutting down'
-                return
+                break
             self.socket.sendto(frame, self.address)
+        log('Shutting down client on %s:%s' % self.address)
+        self.stop()
+
+    def feed_frame(self, frame):
+        self.mailbox.put(frame)
+
+    def set_last_req_time(self):
+        self.last_req_time = datetime.now()
 
     def stop(self):
-        active_clients.remove(self.mailbox)
+        del active_udp_clients[self.address]
         self.mailbox.put("shutdown")
-        self.join()
 
 class VideoCaster(Thread):
     def __init__(self):
@@ -44,10 +55,11 @@ class VideoCaster(Thread):
             if capture_success:
                 encode_success, encoded_frame = cv2.imencode('.jpg', frame, encode_params)
                 if encode_success:
-                    for q in active_clients:
-                        q.put(encoded_frame)
+                    for address, client in active_udp_clients.iteritems():
+                        client.feed_frame(encoded_frame)
 
     def stop(self):
+        self.capture.release()
         self.join()
 
 def server():
@@ -56,22 +68,28 @@ def server():
         socket.SOCK_DGRAM
     )
     server_host = (SERVER_ADDRESS, UDP_PORT)
-    print >>sys.stderr, 'Starting up on %s port %s\n' % server_host
+    log('Starting up on %s port %s\n' % server_host)
     udp_socket.bind(server_host)
 
     caster = VideoCaster()
     caster.daemon = True
     caster.start()
     
+    log('Waiting to receive client request...\n')
     while True:
-        print >>sys.stderr, 'Waiting to receive client request...\n'
         data, address = udp_socket.recvfrom(4096)
 
-        print >>sys.stderr, 'Received a connection request from %s on port %s' % address
-        print address
-        new_client = UDPClient(udp_socket, address)
-        new_client.daemon = True
-        new_client.start()
+        if (active_udp_clients.has_key(address)):
+            log('Received a refresh request from %s on port %s' % address)
+            active_udp_clients[address].set_last_req_time()
+        else:
+            log('Received a connection request from %s on port %s' % address)
+            new_client = UDPClient(udp_socket, address)
+            new_client.daemon = True
+            new_client.start()
+
+def log(message):
+    print >>sys.stderr, '[%s] - %s' % (datetime.now().strftime('%H:%M:%S'), message)
 
 if __name__ == "__main__":
     server()
